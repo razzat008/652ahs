e49696a9c9eeccdc1ab627ada4f7cb62ea9e7514
Razzat Dahal <rajat4623@student.ku.edu.np>
2024-06-28 18:01:34 +0545

    added: templates for better debugging

--- libs/config.h
+++ libs/config.h
@@ -2,7 +2,29 @@
 #include <cstdint>
 #include <cstring>
 #include <fstream>
+#include <functional>
 #include <iomanip>
 #include <iostream>
 #include <sstream>
 #include <vector>
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <GLES2/gl2.h>
+#include <GLFW/glfw3.h>
+#include <sqlite3.h>
+
+// namespace {
+// char *file_name = "hashfile_db";
+// }
+
+/*  Template to show error for every if conditions
+    To use:
+    showerror<const char *, int>(__FILE__, __LINE__);
+    inside the if conditions*/
+template <typename T, typename U> inline void showerror(T file, U line) {
+
+  std::cerr << " FILE: [ " << file << " ] At line: [ " << line - 1
+            << " ]\n Error Message:" << std::endl;
+}

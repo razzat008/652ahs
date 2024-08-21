d6c3d4a7c60dc0204dffec519fc363aaf07c6dd8
Razzat <radiusrazzat111@gmail.com>
2024-08-21 20:04:17 +0545

    added: added function to auto build/compile & run the project

--- install.sh
+++ install.sh
@@ -2,14 +2,30 @@
 install_debian_packages() {
     echo "Installing packages for Debian-based systems..."
     sudo apt update || { echo "Failed to update package list"; exit 2; }
-    sudo apt install -y mesa-utils xorg-dev libxkbcommon-dev libgl1-mesa-dev libglu1-mesa-dev libglfw3-dev libsqlite3-dev || { echo "Failed to install packages"; exit 3; }
+    sudo apt install -y mesa-utils xorg-dev libxkbcommon-dev libgl1-mesa-dev libglu1-mesa-dev libglfw3-dev libsqlite3-dev cmake || { echo "Failed to install packages"; exit 3; }
     echo "Installation successful"
+    compile_build
 }
 
 install_arch_packages() {
     echo "Installing packages for Arch-based systems..."
-    sudo pacman -S --noconfirm --needed sqlite3 glfw mesa mesa-utils || { echo "Failed to install packages"; exit 3; }
+    sudo pacman -S --noconfirm --needed sqlite3 glfw mesa mesa-utils cmake || { echo "Failed to install packages"; exit 3; }
     echo "Installation successful"
+    compile_build
+}
+
+compile_build(){
+  echo "Compiling..."
+  cmake -S . -B build/
+  if [[ $? -eq 0 ]]; then
+    cd build/
+    if [[ $? -eq 0 ]]; then
+      make
+      if [[ $? -eq 0 ]]; then
+        ./652ahs
+      fi
+    fi
+  fi
 }
 
 if [ "$(id -u)" != "0" ]; then
